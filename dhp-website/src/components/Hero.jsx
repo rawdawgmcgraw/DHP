cfee874582e39dc2492ef9ae95c7ccc0e6f819a6
Jim <hippiejim2896@gmail.com>
2024-08-06 03:14:18 -0500

    Style social icons
    Co-authored-by: bgilross <bgilross@users.noreply.github.com>

--- dhp-website/src/components/Hero.jsx
+++ dhp-website/src/components/Hero.jsx
@@ -7,6 +7,9 @@ import ytMusic from "../images/youtube-music.svg";
 import applemusic from "../images/apple-music.svg";
 import mail from "../images/mail.svg";
 
+const iconStyle =
+  "xl:h-24 xl:w-24 lg:h-18 lg:w-18 md:h-12 md:w-12 2xl:mx-8 2xl:my-8 xl:mx-4 xl:my-4 mx-2 my-2 col-span-1 row-span-1 hover:scale-110 ease-in duration-200 cursor-pointer";
+
 const Hero = () => {
   return (
     <div className='flex flex-col h-screen w-full text-[4.5rem] font-retro py-12 items-center'>
@@ -14,49 +17,17 @@ const Hero = () => {
         Dusty Haze Project
       </h1>
       <div className='photo flex h-screen w-full mt-8 items-center justify-between'>
-        <div className='grid grid-cols-2 grid-rows-2 ml-28'>
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={facebook}
-            alt='Facebook'
-          />
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={insta}
-            alt='Instagram'
-          />
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={tiktok}
-            alt='TikTok'
-          />
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={mail}
-            alt='E-Mail'
-          />
+        <div className='grid grid-cols-2 grid-rows-2 2xl:ml-28 xl:mx-12'>
+          <img className={iconStyle} src={facebook} alt='Facebook' />
+          <img className={iconStyle} src={insta} alt='Instagram' />
+          <img className={iconStyle} src={tiktok} alt='TikTok' />
+          <img className={iconStyle} src={mail} alt='E-Mail' />
         </div>
-        <div className='grid grid-cols-2 grid-rows-2 mr-28'>
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={spotify}
-            alt='Spotify'
-          />
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={youtube}
-            alt='Youtube'
-          />
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={ytMusic}
-            alt='YouTube Music'
-          />
-          <img
-            className='h-24 w-24 mx-4 col-span-1 row-span-1 my-4 hover:scale-110 ease-in duration-200 cursor-pointer'
-            src={applemusic}
-            alt='AppleMusic'
-          />
+        <div className='grid grid-cols-2 grid-rows-2 2xl:mr-28 xl:mx-12'>
+          <img className={iconStyle} src={spotify} alt='Spotify' />
+          <img className={iconStyle} src={youtube} alt='Youtube' />
+          <img className={iconStyle} src={ytMusic} alt='YouTube Music' />
+          <img className={iconStyle} src={applemusic} alt='AppleMusic' />
         </div>
       </div>
     </div>
