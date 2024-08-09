befb30e8a17dc3ce0f4c7425362934a96d2eb954
Jim <hippiejim2896@gmail.com>
2024-08-09 00:57:37 -0500

    Continue working on PicCollage styling

--- dhp-website/src/components/PicCollage.jsx
+++ dhp-website/src/components/PicCollage.jsx
@@ -1,28 +1,77 @@
-import d1 from "../images/dhp1.jfif";
-// import d2 from "../images/dhp2.jfif";
-// import d3 from "../images/dhp3.jfif";
 import d4 from "../images/dhp4.jfif";
-// import d5 from "../images/dhp5.jfif";
-// import d6 from "../images/dhp6.jpg";
-// import d7 from "../images/dhp7.jpg";
-// import d8 from "../images/dhp8.jpg";
-// import d9 from "../images/dhp9.jpg";
-// import ds from "../images/dhpSized.png";
-// import ds1 from "../images/dhpSized1.png";
-// import ds2 from "../images/dhpSized2.png";
-// import ds3 from "../images/dhpSized3.png";
-// import ds4 from "../images/dhpSized4.png";
-// import ds5 from "../images/dhpSized5.png";
+import d5 from "../images/dhp5.jpg";
+import ds from "../images/dhpSized.png";
+import ds1 from "../images/dhpSized1.png";
+import ds2 from "../images/dhpSized2.png";
+import ds3 from "../images/dhpSized3.png";
+import ds4 from "../images/dhpSized4.png";
+import ds5 from "../images/dhpSized5.png";
+import ds6 from "../images/dhpSized6.png";
 
 const PicCollage = () => {
   return (
     <div className='flex flex-col h-screen w-full items-center'>
-      <div className='mt-4'>
-        <img src={d4} alt='' />
+      <div className='my-8'>
+        <img className='rounded-3xl' src={d4} alt='' />
       </div>
-      <div className='flex max-w-[720px] items-center justify-between'>
-        <div>
-          <img src={d1} alt='' />
+      <div className='flex max-w-[720px] items-center'>
+        <div className='grid grid-cols-4 grid-rows-2 place-items-center gap-4'>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds1}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds2}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds3}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds4}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds5}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={ds6}
+              alt=''
+            />
+          </div>
+          <div className='shadow-md shadow-yellow-400 rounded-full'>
+            <img
+              className='rounded-full hover:opacity-60 cursor-pointer'
+              src={d5}
+              alt=''
+            />
+          </div>
         </div>
       </div>
     </div>
